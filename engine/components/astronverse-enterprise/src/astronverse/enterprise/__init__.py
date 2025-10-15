077bff9ce2ca9fdf264d6c6bbf9e5a9d08fbd8c8
blessleon <blessleon@qq.com>
2025-10-15 14:05:21 +0800

    chore: Enterprise pylint

--- engine/components/astronverse-enterprise/src/astronverse/enterprise/__init__.py
+++ engine/components/astronverse-enterprise/src/astronverse/enterprise/__init__.py
@@ -1,7 +1,10 @@
+""" Enterprise module initialization """
 from enum import Enum
 
 
 class ReportLevelType(Enum):
+    """ Report level types """
+    
     INFO = "info"
     WARNING = "warning"
     ERROR = "error"
