001bc1075c19b3e79d8f9b60d61a21fdbd297618
sjuartwey <15555099530@163.com>
2025-10-16 17:38:30 +0800

    chore: format

--- backend/robot-service/src/main/java/com/iflytek/rpa/base/constants/BaseConstant.java
+++ backend/robot-service/src/main/java/com/iflytek/rpa/base/constants/BaseConstant.java
@@ -27,16 +27,16 @@ public class BaseConstant {
 
     public static final Integer MAX_PROCESS_SIZE = 14;
 
-    public static final String MODULE_INIT_CONTENT = "from typing import Any\n" +
-            "from astronverse.workflowlib.helper import Helper, print, logger\n" +
-            "\n" +
-            "\n" +
-            "def main(*args, **kwargs) -> Any:\n" +
-            "    h = Helper(**kwargs)\n" +
-            "    params = h.params()\n" +
-            "\n" +
-            "    # 打印所有的变量key\n" +
-            "    logger.info(params.keys())\n" +
-            "\n" +
-            "    return True";
+    public static final String MODULE_INIT_CONTENT =
+            "from typing import Any\n" + "from astronverse.workflowlib.helper import Helper, print, logger\n"
+                    + "\n"
+                    + "\n"
+                    + "def main(*args, **kwargs) -> Any:\n"
+                    + "    h = Helper(**kwargs)\n"
+                    + "    params = h.params()\n"
+                    + "\n"
+                    + "    # 打印所有的变量key\n"
+                    + "    logger.info(params.keys())\n"
+                    + "\n"
+                    + "    return True";
 }
