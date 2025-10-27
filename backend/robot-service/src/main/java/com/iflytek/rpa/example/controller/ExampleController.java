94529440b46f28be01a8158e5c84812504fbeaa3
sjuartwey <15555099530@163.com>
2025-10-27 14:58:06 +0800

    chores: format

--- backend/robot-service/src/main/java/com/iflytek/rpa/example/controller/ExampleController.java
+++ backend/robot-service/src/main/java/com/iflytek/rpa/example/controller/ExampleController.java
@@ -1,9 +1,9 @@
 package com.iflytek.rpa.example.controller;
 
 import com.iflytek.rpa.starter.exception.ServiceException;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/example")
@@ -12,14 +12,12 @@ public class ExampleController {
     @PostMapping("/insert")
     public void insertExample(@RequestBody Map<String, Object> requestBody) {
         log.info("received hook with request body: {}", requestBody);
-        
+
         // 提取organization字段
         if (!requestBody.containsKey("organization") || !requestBody.containsKey("user"))
             throw new ServiceException("hook body from cas-door is missing");
 
         String user = (String) requestBody.get("user");
         String organization = (String) requestBody.get("organization");
-
-        log.info(user + ":" + organization);
     }
 }
