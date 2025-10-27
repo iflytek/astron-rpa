fb406d318fd4fa0a02a4887868665d67ef52f1fe
sjuartwey <15555099530@163.com>
2025-10-27 14:46:08 +0800

    feat: get info from casdoor hook request body

--- backend/robot-service/src/main/java/com/iflytek/rpa/example/controller/ExampleController.java
+++ backend/robot-service/src/main/java/com/iflytek/rpa/example/controller/ExampleController.java
@@ -1,16 +1,25 @@
 package com.iflytek.rpa.example.controller;
 
+import com.iflytek.rpa.starter.exception.ServiceException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/example")
 @Slf4j
 public class ExampleController {
-    @GetMapping("/insert")
-    public void insertExample() {
-        log.info("received hook");
+    @PostMapping("/insert")
+    public void insertExample(@RequestBody Map<String, Object> requestBody) {
+        log.info("received hook with request body: {}", requestBody);
+        
+        // 提取organization字段
+        if (!requestBody.containsKey("organization") || !requestBody.containsKey("user"))
+            throw new ServiceException("hook body from cas-door is missing");
+
+        String user = (String) requestBody.get("user");
+        String organization = (String) requestBody.get("organization");
+
+        log.info(user + ":" + organization);
     }
 }
