20946f763dc0d11fd5ea9259bcc0ba8e7fd00248
horizon220222 <1820120473@qq.com>
2025-12-31 10:04:09 +0800

    feat: Add excel v2

---
+++ engine/components/astronverse-excel/src/astronverse/excel/error.py
@@ -0,0 +1,5 @@
+from astronverse.baseline.error.error import BaseException, BizCode, ErrorCode
+from astronverse.baseline.i18n.i18n import _
+
+BaseException = BaseException
+
---
+++ engine/components/astronverse-excel/src/astronverse/excel/excel_obj.py
@@ -0,0 +1,29 @@
+import sys
+from typing import Any
+from astronverse.actionlib.types import typesMg
+from astronverse.actionlib.error import PARAM_VERIFY_ERROR_FORMAT
+from astronverse.excel.error import *
+
+
+class ExcelObj:
+    """Excel对象"""
+
+    def __init__(self, obj: Any, path: str = ""):
+        self.obj = obj
+        self.path = path
+
+    @classmethod
+    def __validate__(cls, name: str, value):
+        if isinstance(value, ExcelObj):
+            return value
+        raise BaseException(PARAM_VERIFY_ERROR_FORMAT.format(name, value), "{}参数验证失败{}".format(name, value))
+
+    def get_name(self):
+        if sys.platform == "win32":
+            return self.obj.Name
+        else:
+            return ""
+
+    @typesMg.shortcut("ExcelObj", res_type="Str")
+    def get_full_name(self) -> str:
+        return self.path if self.path else self.get_name()
