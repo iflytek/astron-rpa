8246da7f4efb18f0b7c66edef79892d23e6b81d3
blessleon <blessleon@qq.com>
2026-01-21 21:35:33 +0800

    fix: config edit

--- engine/components/astronverse-browser/src/astronverse/browser/browser_element.py
+++ engine/components/astronverse-browser/src/astronverse/browser/browser_element.py
@@ -1249,7 +1249,7 @@ class BrowserElement:
                 dynamics=[
                     DynamicsItem(
                         key="$this.attribute_name.show",
-                        expression=f"return ['{ElementGetAttributeTypeFlag.GetAttribute.value}', '{ElementGetAttributeTypeFlag.GetStyle.value}'].includes($this.get_type.value) || ['{ElementAttributeOpTypeFlag.Set.value}', '{ElementAttributeOpTypeFlag.Del.value}'].includes($this.operation_type.value)",
+                        expression="return ['getAttribute','getStyle'].includes($this.get_type.value) || ['set', 'del'].includes($this.operation_type.value)",
                     )
                 ],
             ),
